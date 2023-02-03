29ff6759d02c24dc69f79ecce3b7753feece8735
danielbeilin <dbeilin3@gmail.com>
2023-02-03 13:31:22 +0200

    ground alignment

--- Assets/Scripts/Mechanics/KinematicObject.cs
+++ Assets/Scripts/Mechanics/KinematicObject.cs
@@ -167,7 +167,13 @@ namespace Platformer.Mechanics
                             velocity = velocity - projection * currentNormal;
                         }
                     }
-                    //remove shellDistance from actual move distance.
+/*                  else
+                    {
+                        //We are airborne, but hit something, so cancel vertical up and horizontal velocity.
+                        velocity.x *= 0;
+                        velocity.y = Mathf.Min(velocity.y, 0);
+                    }
+*/                    //remove shellDistance from actual move distance.
                     var modifiedDistance = hitBuffer[i].distance - shellRadius;
                     distance = modifiedDistance < distance ? modifiedDistance : distance;
 
